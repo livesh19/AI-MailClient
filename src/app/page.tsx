8132ec3ee1e2d2fe85826e40026c9e41d46cb583
livesh19 <livesh1901@gmail.com>
2025-04-06 21:38:50 +0530

    checkBox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import LinkAccountButton from "@/components/link-account-button";
 import { Button } from "@/components/ui/button";
 
 
+
 export default async function Home() {
   return <LinkAccountButton />
  
